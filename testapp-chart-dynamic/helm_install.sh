19275ad59ef3312f35b981985d22c1ac32094259
bertei <bteisceira@gmail.com>
2022-09-01 19:14:49 -0300

    Added ingress

--- testapp-chart-dynamic/helm_install.sh
+++ testapp-chart-dynamic/helm_install.sh
@@ -1,8 +1,7 @@
 helm install -f testapp-chart-dynamic/values/testapp_db-values.yml testapp-db-microservice testapp-chart-dynamic
 helm install -f testapp-chart-dynamic/values/testapp_backend-values.yml testapp-backend-microservice testapp-chart-dynamic
 helm install -f testapp-chart-dynamic/values/testapp_frontend-values.yml testapp-frontend-microservice testapp-chart-dynamic
-kubectl port-forward testapp-db-service 5432:5432
-kubectl port-forward testapp-frontend-service 8000:8000
-kubectl port-forward testapp-frontend-service 3000:3000
 kubectl get all
 helm list
+curl testapp.dev/frontend
+curl testapp.dev/backend
