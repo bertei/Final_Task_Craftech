beab8f1782b8c75174f80ccf8a5c0aa2522bf94e
bertei <bteisceira@gmail.com>
2022-09-01 15:58:58 -0300

    Helm test

--- testapp-chart-dynamic/helm_install.sh
+++ testapp-chart-dynamic/helm_install.sh
@@ -1,3 +1,8 @@
-helm install -f values/testapp_db-values.yml testapp-db-microservice ../testapp-chart-dynamic
-helm install -f values/testapp_backend-values.yml testapp-backend-microservice ../testapp-chart-dynamic
-helm install -f values/testapp_frontend-values.yml testapp-frontend-microservice ../testapp-chart-dynamic
+helm install -f testapp-chart-dynamic/values/testapp_db-values.yml testapp-db-microservice testapp-chart-dynamic
+helm install -f testapp-chart-dynamic/values/testapp_backend-values.yml testapp-backend-microservice testapp-chart-dynamic
+helm install -f testapp-chart-dynamic/values/testapp_frontend-values.yml testapp-frontend-microservice testapp-chart-dynamic
+kubectl port-forward testapp-db-service 5432:5432
+kubectl port-forward testapp-frontend-service 8000:8000
+kubectl port-forward testapp-frontend-service 3000:3000
+kubectl get all
+helm list
