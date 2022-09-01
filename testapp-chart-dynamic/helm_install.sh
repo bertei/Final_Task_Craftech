61e06d468c867ed7f20eaca54cc258958d807bf2
bertei <bteisceira@gmail.com>
2022-09-01 19:41:10 -0300

    Trying to fix helm chart ingress error

--- testapp-chart-dynamic/helm_install.sh
+++ testapp-chart-dynamic/helm_install.sh
@@ -1,7 +1,9 @@
 helm install -f testapp-chart-dynamic/values/testapp_db-values.yml testapp-db-microservice testapp-chart-dynamic
 helm install -f testapp-chart-dynamic/values/testapp_backend-values.yml testapp-backend-microservice testapp-chart-dynamic
 helm install -f testapp-chart-dynamic/values/testapp_frontend-values.yml testapp-frontend-microservice testapp-chart-dynamic
+kubectl install -f ./testapp-ingress.yml
 kubectl get all
 helm list
 curl testapp.dev/frontend
+sleep 3
 curl testapp.dev/backend
