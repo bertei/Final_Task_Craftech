22165aebbe6e578ff0aa3928135cd8ec476b20e7
bertei <bteisceira@gmail.com>
2022-09-06 09:59:00 -0300

    Helm chart with ingress test 11

--- testapp-chart-dynamic/helm_install.sh
+++ testapp-chart-dynamic/helm_install.sh
@@ -2,13 +2,13 @@ helm install -f testapp-chart-dynamic/values/testapp_db-values.yml testapp-db-mi
 helm install -f testapp-chart-dynamic/values/testapp_backend-values.yml testapp-backend-microservice testapp-chart-dynamic
 helm install -f testapp-chart-dynamic/values/testapp_frontend-values.yml testapp-frontend-microservice testapp-chart-dynamic
 kubectl apply -f testapp-chart-dynamic/testapp-ingress.yml
-kubectl get all
-curl testapp.dev/frontend
-sleep 3
-curl testapp.dev/backend
-sleep 3
 minikube service list
 minikube service testapp-frontend-service --url
 echo "------------------opening the service------------------"
 curl $(minikube service testapp-frontend-service --url)
-
+minikube service testapp-backend-service --url
+echo "------------------opening the service------------------"
+curl $(minikube service testapp-backend-service --url)
+echo "------------------Testing ingress------------------"
+curl $(minikube ip)/frontend
+curl $(minikube ip)/backend
